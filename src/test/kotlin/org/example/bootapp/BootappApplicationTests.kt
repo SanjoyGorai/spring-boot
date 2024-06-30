7d232fc4cb7b3e2ea54175ac964173294005d825
Mr. Sanjoy <--add>
2024-06-30 08:30:51 +0530

    first commit

---
+++ src/test/kotlin/org/example/bootapp/BootappApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.bootapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootappApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
